d7a41dff0130406397b9c6b68af3131592123e2d
clairenicholsondigital <139775904+clairenicholsondigital@users.noreply.github.com>
2023-07-21 11:47:25 +0100

    update to functionality of dropdown

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let london = document.querySelector("#london");
+let london = document.querySelector("#london");
 let tokyo = document.querySelector("#tokyo");
 let paris = document.querySelector("#paris");
 let sydney = document.querySelector("#sydney");
@@ -22,26 +21,34 @@ function showAllCountries() {
 setInterval(showAllCountries, 1000);
 
 
-let localTimezone = moment.tz.guess();
-let localElement = document.querySelector("#local");
-localElement.innerHTML = `${localTimezone} and the current time is ${moment().format(
-  "h:m A"
-)}`;
-let countriesSelect = document.querySelector("#countries");
+////let localTimezone = moment.tz.guess();
+//let localElement = document.querySelector("#local");
+//localElement.innerHTML = `${localTimezone} and the current time is ${moment().format(
+ // "h:m A"
+//)}`;
 
+let countriesSelect = document.querySelector(".countries select");
 
 function showSelectedTime() {
-    let selectedCountry = countriesSelect.value;
-    if (selectedCountry === "London") {
-        alert("LONDON");
-    } else if (selectedCountry === "Paris") {
-        alert("PARIS");
-    } else if (selectedCountry === "Local") {
-        alert("LOCAL");
-    } else if (selectedCountry === "Tokyo") {
-        alert("TOKYO");
-    }
+  let countriesSelect = document.querySelector(".countries select");
+  let selectedCountry = countriesSelect.value;
+  let citiesElement = document.querySelector(".countrieslist");
+
+  if (selectedCountry === "London") {
+    let londonTime = moment().tz("Europe/London").format("dddd DD MMMM YYYY");
+    citiesElement.innerHTML = `<span class='countries'>London <img src="https://images.emojiterra.com/twitter/v14.0/1024px/1f1ec-1f1e7.png" width="30px"/></span>&nbsp;` + londonTime;
+    citiesElement.innerHTML += `<span class='time'>${moment().tz("Europe/London").format("h:mm A")}</span>`;
+  } else if (selectedCountry === "Paris") {
+    let ParisTime = moment().tz("Europe/Paris").format("dddd DD MMMM YYYY");
+    citiesElement.innerHTML = `<span class='countries'>Paris<img src="https://em-content.zobj.net/thumbs/240/apple/354/flag-france_1f1eb-1f1f7.png" width="30px"/></span>&nbsp;` + ParisTime;
+    citiesElement.innerHTML += `<span class='time'>${moment().tz("Europe/Paris").format("h:mm A")}</span>`;
+  } else if (selectedCountry === "Local") {
+  } else if (selectedCountry === "Tokyo") {
+  let TokyoTime = moment().tz("Asia/Tokyo").format("dddd DD MMMM YYYY");
+   citiesElement.innerHTML = `<span class='countries'>Tokyo<img src="https://em-content.zobj.net/thumbs/240/apple/354/flag-japan_1f1ef-1f1f5.png" width="30px"/></span>&nbsp;` + TokyoTime;
+   ciitiesElement.innerHTML += `<span class='time'>${moment().tz("Asia/Tokyo").format("h:mm A")}</span>`;  
+  }
 }
 
-  countriesSelect.addEventListener("change", showSelectedTime);
-});
+
+countriesSelect.addEventListener("change", showSelectedTime);
